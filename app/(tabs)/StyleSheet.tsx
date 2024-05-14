65364d51b73e819c6193385b55f00411abcb3afd
Bruno Ferreira da Silva <bruno_silva2711@yahoo.com.br>
2024-05-13 21:37:48 -0300

    Criação do app lembretes

---
+++ app/(tabs)/StyleSheet.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    telaPrincipalView: {
+        padding: 50
+    },
+    lembreteView: {
+        flexDirection: 'row', 
+        justifyContent: 'space-between', 
+        alignItems: "center"
+    },
+    lembreteInputText: { 
+        width: '80%', 
+        borderBottomColor: 'black', 
+        borderBottomWidth: 1,
+        padding: 2
+    }
+});
